e0ced20c1f402627172939cc9ff95ac219300703
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-08-12 19:34:15 -0600

    update navigation and branding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function NavLink({ href, children }) {
   return (
     <Link
       href={href}
-      className="transition hover:text-teal-500 dark:hover:text-teal-400"
+      className="transition hover:text-red-500 dark:hover:text-red-500"
     >
       {children}
     </Link>
@@ -21,10 +21,9 @@ export function Footer() {
           <Container.Inner>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                <NavLink href="/about">About</NavLink>
-                <NavLink href="/articles">Articles</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                <NavLink href="/blog">Blog</NavLink>
+                <NavLink href="/progress">Progress</NavLink>
+                <NavLink href="/races">Races</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Kyle Kearney. All rights
--- src/pages/progress.jsx
+++ src/pages/progress.jsx
@@ -1,47 +1,71 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import clsx from 'clsx'
-
 import { Container } from '@/components/Container'
 import {
-  GitHubIcon,
-  InstagramIcon,
-  LinkedInIcon,
-  TwitterIcon,
-} from '@/components/SocialIcons'
-import portraitImage from '../../public/images/portrait.jpg'
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
 
-function SocialLink({ className, href, children, icon: Icon }) {
-  return (
-    <li className={clsx(className, 'flex')}>
-      <Link
-        href={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
-      >
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
-        <span className="ml-4">{children}</span>
-      </Link>
-    </li>
-  )
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Per Mile Pace',
+    },
+  },
+};
+
+const labels = ['7/29/23']
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Biking',
+      data: [3.22],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+    {
+      label: 'Running',
+      data: [9.51],
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Swimming',
+      data: [26.5],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
 }
 
-function MailIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
-      />
-    </svg>
-  )
-}
-
-export default function About() {
+export default function Progress() {
   return (
     <>
       <Head>
-        <title>About - Kyle</title>
+        <title>Progress</title>
         <meta
           name="description"
           content="I’m Kyle. I write clean code, read economics book and obsess over pixels."
@@ -49,42 +73,12 @@ export default function About() {
       </Head>
       <Container className="mt-16 sm:mt-32">
         <div className="grid grid-cols-1 gap-y-8">
-            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl w-3/4">
-              I’m Kyle. I write clean code, read economics book and obsess over
-              pixels.
-            </h1>
-            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-              <p>
-                I am a front end engineer with 3+ years of experience. I have
-                worked in a wide array of team environments, tech stacks and
-                application types. From leading the web marketing effort at a
-                multi billion dollar conglomerate, to wearing all the hats at
-                small start-ups.
-              </p>
-              <p>
-                I spent two and a half years growing at Lead Sherpa. I spent the
-                time perfecting my craft with React and learning the ins and out
-                of managing a product and more importantly building a team. My
-                role shifted from from frontend engineer, to lead engineer,
-                ending with technical product owner. While I do enjoy the
-                business aspect of the product role, solving problems and
-                working along side my fellow engineer truly brings me great joy.
-              </p>
-              <p>
-                Minimalism and simplicity are key values in every aspect of my
-                life, especially development. Clean, readable code and quality
-                test coverage are the fundamental attributes to a quality
-                application. I primarily code in Javascript and Ruby, but have
-                experience with a few other languages.
-              </p>
-              <p>
-                When I am not working you can find me, cooking, learning or
-                training for my first triathlon. I am a life long learner, AFOL
-                and personal finance nerd, You can checkout my current
-                projects on my GitHub. If you have a question, looking for
-                a dev, or a friend feel free to reach out.
-              </p>
-            </div>
+          <h1 className="w-3/4 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Simple progress tracker to track key stats and figures.
+          </h1>
+          <div className="mt-6 w-full space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <Line options={options} data={data} />
+          </div>
         </div>
       </Container>
     </>
